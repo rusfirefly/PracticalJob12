e1bcdc951e80fe11e04952b0984adbc9d9f1cb68
rusfirefly <fesov_86@mail.ru>
2024-10-03 16:14:39 +0300

    update

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -1,7 +1,31 @@
+using DG.Tweening;
 using UnityEngine;
 
-public abstract class Trap : MonoBehaviour
+public class Trap : MonoBehaviour
 {
-    [SerializeField] private bool _isActive;
+    [SerializeField] Transform[] _positions;
+    [SerializeField] private bool _isStart;
+
+    private int _positionIndex = 0;
+    private Vector3 _position;
+    private bool _isEnd;
+
+    private void Update()
+    {
+        if(_isStart == false) return;
+
+        Mathf.Clamp(_positionIndex++,0, _positions.Length);
+
+        Debug.Log(_positionIndex);
+    }
+
+    public void Enabele() => _isStart = true;
+
+    public void Disabele() => _isStart = false;
+
+    private Vector3 NextPoint() => _positions[_positionIndex++].transform.position;
+
+    private Vector3 PreviousPoint()=> _positions[_positionIndex--].transform.position;
+
 
 }
