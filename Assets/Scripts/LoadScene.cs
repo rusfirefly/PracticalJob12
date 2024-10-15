71303b597a0066e5f14e86b96dc8fff73f3decff
rusfirefly <fesov_86@mail.ru>
2024-10-15 15:54:08 +0300

    update

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+
+    private void Awake()
+    {
+        Load();
+    }
+
+    public void Load()
+    {
+        StartCoroutine(AsynkLoad());
+    }
+
+    private IEnumerator AsynkLoad()
+    {
+        AsyncOperation asynkLoad = SceneManager.LoadSceneAsync(_sceneName);
+
+        while (asynkLoad.isDone == false)
+        {
+            yield return null;
+        }
+    }
+}
